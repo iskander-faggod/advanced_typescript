2d15df6c3c6d5efeb3f325d30e0ada86f01d4f99
Iskander <grinerplaydark@gmail.com>
2021-06-22 17:49:59 +0300

    finish generics

--- dist/generic.js
+++ dist/generic.js
@@ -1,5 +1,6 @@
 "use strict";
 const cars = ['ford', 'audi'];
+//Promises
 const promise = new Promise(resolve => {
     setTimeout(() => {
         resolve('Promise');
@@ -8,9 +9,45 @@ const promise = new Promise(resolve => {
 promise.then(data => {
     console.log(data);
 });
+//Functions
 function mergeObjects(a, b) {
     return Object.assign({}, a, b);
 }
 const merged = mergeObjects({ name: 'Iskander' }, { age: 18 });
 console.log(merged);
+const withCount = (value) => ({ value, count: value.length });
+function getObjByValue(obj, key) {
+    return obj[key];
+}
+//Classes
+class Collection {
+    constructor(_items = []) {
+        this._items = _items;
+    }
+    add(item) {
+        this._items.push(item);
+    }
+    remove(item) {
+        this._items = this._items.filter(i => i != item);
+    }
+    get items() {
+        return this._items;
+    }
+}
+const strings = new Collection(['I', 'am', 'strings']);
+strings.add('NO!');
+console.log(strings.items);
+const nums = new Collection([1, 2, 3]);
+nums.add(1);
+console.log(nums.items);
+function createAndValidateCar(model, year) {
+    const car = {};
+    if (model.length > 3) {
+        car.model = model;
+    }
+    if (year > 2000) {
+        car.year = year;
+    }
+    return car;
+}
 //# sourceMappingURL=generic.js.map
--- src/generic.ts
+++ src/generic.ts
@@ -1,5 +1,6 @@
 const cars: string[] = ['ford', 'audi']
 
+//Promises
 const promise = new Promise<string>(resolve => {
     setTimeout(() => {
         resolve('Promise')
@@ -10,7 +11,7 @@ promise.then(data => {
     console.log(data)
 })
 
-
+//Functions
 function mergeObjects<T, R>(a: T, b: R): T & R {
     return Object.assign({}, a, b)
 }
@@ -24,3 +25,49 @@ const withCount = (value: string | any[]) => ({value, count: value.length});
 function getObjByValue<T extends object, R extends keyof T>(obj: T, key: R) {
     return obj[key]
 }
+
+//Classes
+class Collection<T> {
+    constructor(private _items: T[] = []) {
+    }
+
+    add(item: T) {
+        this._items.push(item)
+    }
+
+    remove(item: T) {
+        this._items = this._items.filter(i => i != item)
+    }
+
+    get items(): T[] {
+        return this._items
+    }
+}
+
+const strings = new Collection<string>(['I', 'am', 'strings'])
+strings.add('NO!')
+console.log(strings.items)
+
+const nums = new Collection<number>([1, 2, 3])
+nums.add(1)
+console.log(nums.items)
+
+
+//Interfaces
+interface Car {
+    model: string
+    year: number
+}
+
+function createAndValidateCar(model: string, year: number): Car {
+    const car: Partial<Car> = {}
+    if (model.length > 3) {
+        car.model = model
+    }
+    if (year > 2000) {
+        car.year = year
+    }
+
+    return car as Car
+}
+
